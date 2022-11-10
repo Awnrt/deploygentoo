be7e83d044961c360972de85a1c975edbc0c6cd4
Awnrt <roman.levikoff@gmail.com>
2022-11-10 22:28:11 +0300

    upload

--- post_chroot.sh
+++ post_chroot.sh
@@ -10,6 +10,28 @@ locale-gen
 eselect locale set 4
 
 env-update && source /etc/profile
-
+rm -f /etc/portage/package.license
+cd /etc/portage/
+wget https://raw.githubusercontent.com/Awnrt/deploygentoo/main/portage/package.license
 emerge sys-kernel/linux-firmware sys-apps/pciutils
+
+emerge sys-kernel/gentoo-kernel-bin
+
+echo "/dev/$disk_drive"1"               /boot           ext4            noauto,noatime  1 2" >> /etc/fstab
+echo "/dev/$disk_drive"3"               /               ext4            noatime         0 1" >> /etc/fstab
+echo "/dev/$disk_drive"2"               none            swap            sw              0 0" >> /etc/fstab
+
+echo hostname=\"$_hostname\" > /etc/conf.d/hostname
+
+emerge net-misc/dhcpcd netifrc
+
+rc-update add dhcpcd default
+echo config_eno1=\"dhcp\" > /etc/conf.d/net
+
+cd /etc/init.d/
+ln -s net.lo net.eno1
+rc-update add net.eno1 default
+emerge sys-boot/grub
+grub-install --target=x86_64-efi --efi-directory=/boot
+grub-mkconfig -o /boot/grub/grub.cfg
 
