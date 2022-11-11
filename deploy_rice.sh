1a7ff12f9334a53bc62a0c245a05bb6443907b13
Awnrt <roman.levikoff@gmail.com>
2022-11-11 03:29:33 +0300

    upload

--- deploy_rice.sh
+++ deploy_rice.sh
@@ -1,14 +1,27 @@
-DEPLIST="`sed -e 's/#.*$//' -e '/^$/d' dependencies.txt | tr '\n' ' '`"
-doas emerge --autounmask-continue -q $DEPLIST
+emerge --unmerge dev-lang/rust
+emerge --oneshot virtual/rust dev-lang/rust-bin
+emerge --update --deep @world
+emerge --depclean
+echo 'dev-lang/rust' >> /etc/portage/package.mask
 
-cd $HOME
+DEPLIST="`sed -e 's/#.*$//' -e '/^$/d' dependencies.txt | tr '\n' ' '`"
+sudo emerge --autounmask-continue -q $DEPLIST
+
+cd /home/awy
 git clone https://github.com/awnrt/dwm
 git clone https://github.com/awnrt/dmenu
-cd $HOME/dwm
+cd home/awy/dwm
 make clean install
-cd $HOME/dmenu
+cd /home/awy/dmenu
 make clean install
 
-cd $HOME/deploygentoo/rice/dots
-cp .xinitrc $HOME
-cp .Xresources $HOME
+cd /home/awy/deploygentoo/rice/dots
+cp .xinitrc /home/awy/
+cp .Xresources /home/awy/
+
+sudo mkdir /etc/X11/xorg.conf.d
+
+sudo cp /home/awy/deploygentoo/nvidia/nvidia.conf /etc/X11/xorg.conf.d/nvidia.conf
+sudo cp /home/awy/deploygentoo/nvidia/video.conf /etc/modules-load.d/video.conf
+usermod -a -G video awy
+sudo rc-update add elogind boot
